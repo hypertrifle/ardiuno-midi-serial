c7c1ffd8568a01141276e92479e3a0acb60ecb37
U-DESKTOP-0GHCVNG\rich <ricardo.searle@gmail.com>
2018-01-23 07:35:05 +0000

    WIP from last night - more comments to main class

--- midi_input_tests.ino
+++ midi_input_tests.ino
@@ -1,13 +1,25 @@
 /*
- *******************************************************************************
-   USB-MIDI to Legacy Serial MIDI converter
-   Copyright (C) 2012-2017 Yuuichi Akagawa
+*******************************************************************************
+midi messages come in as 3 bytes each byte can be of values 0 - 127 | 0x00 - 0x7F
 
-   Idea from LPK25 USB-MIDI to Serial MIDI converter
-     by Collin Cunningham - makezine.com, narbotic.com
+byte[0]
+type of message, 
+- 0xB0 seems to be change in a ctrl knob - note we seem to get this changing when not touching the pad (possibly due to noise on the arduino board)
+- 0x99 down on a pad
+- 0x89 up on a pad
+- 0xD9 aftertouch - is there only 1 aftertouch vailbe for all pads?
 
-   This is sample program. Do not expect perfect behavior.
- *******************************************************************************
+byte[1]
+data 1 
+- when a control this is the control number 0x03 is the first control, 0x09 is the second and all are in series after thiss, 0x0a, 0x0b ...
+- when an up or down event this is the pad, based on note (settings will change per device this one has settings for different scales)
+- when after touch this is the value of aftertouch (change in pressure on a pad). 
+
+byte[3]
+data 2
+- when a control this is the value.
+- when down this is the velocity of the hit 
+*******************************************************************************
 */
 
 
@@ -25,48 +37,50 @@
 #else
 #define _MIDI_SERIAL_PORT Serial
 #endif
-//////////////////////////
-// MIDI Pin assign
-// 2 : GND
-// 4 : +5V(Vcc) with 220ohm
-// 5 : TX
-//////////////////////////
 
 USB Usb;
 USBH_MIDI  Midi(&Usb);
 
 const char * notes [] = { "C ", "C#", "D ", "D#", "E ", "F ", "F#", "G ", "G#", "A ", "A#", "B " };
 
-uint8_t controlValues[9];
+const uint8_t control_threshold = 1;
+
+uint8_t controlValues[20];
 
 void MIDI_poll();
+
 void doDelay(uint32_t t1, uint32_t t2, uint32_t delayTime);
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); //for debugging to arduino serial monitor
+  Serial1.begin(31250); 
+  
   Serial.println("begin");
-  Serial1.begin(31250);
 
   if (Usb.Init() == -1) {
-    while (1); //halt
-  }//if (Usb.Init() == -1...
+    while (1);
+  }
   delay( 200 );
 }
 
 void loop()
 {
+
+  //USB stuff
   Usb.Task();
   uint32_t t1 = (uint32_t)micros();
+
   if ( Usb.getUsbTaskState() == USB_STATE_RUNNING )
   {
     MIDI_poll();
   }
-  //delay(1ms)
+
+  //this is make it fixed framerate right?
   doDelay(t1, (uint32_t)micros(), 1000);
 }
 
-// Poll USB MIDI Controler and send to serial MIDI
+// Poll USB MIDI Controler and handle
 void MIDI_poll()
 {
   
@@ -76,48 +90,28 @@ void MIDI_poll()
   do {
     if ( (size = Midi.RecvData(outBuf)) > 0 ) {
       //MIDI Output
-      midiLog(outBuf);
-      
-//      _MIDI_SERIAL_PORT.write(outBuf, size);
+      handleMidiData(outBuf);
     }
   } while (size > 0);
 
   
 }
 
+// this is a way to reduce noise on development boards.
 bool updateControls(uint8_t control, uint8_t newVale){
 
-  if(control == 0x03){
-    if(abs(controlValues[0] - newVale) > 1){
-      controlValues[0] = newVale;
-      return true;
-    }
-  } else {
-    if(abs(controlValues[control - 8] - newVale) > 1){
-      controlValues[control - 8] = newVale;
-      return true;
-    }
-  }
+if(abs(controlValues[control] - newVale) > control_threshold){
+  controlValues[control] = newVale;
+  return true;
+}
 
-  return false;
+ return false;
 }
 
 
 
-void midiLog(uint8_t bytes[]){
-  //so here we ca
-
-
-  /*
-  WHAT WE KNOW SO FAR
-
-  byte[0]
-  type of message, 0xB0 seems to be change in a ctrl knob 
-
-
-
-
-  */
+void handleMidiData(uint8_t bytes[]){
+  
 
   // if(bytes[0] == 0xB0 && bytes[1] == 0x1b)
   //   return;
